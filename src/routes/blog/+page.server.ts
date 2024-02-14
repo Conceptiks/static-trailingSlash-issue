f6d9f489f70b018ef159774bbfa2f04d906b1fba
Conceptiks <maxim.tan@inlume.io>
2024-02-14 15:59:14 +0100

    removed log

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,26 +1,23 @@
-import { slugFromPath } from "$lib/slugFromPath";
-import type { PageServerLoad } from "./$types";
+import { slugFromPath } from '$lib/slugFromPath';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-  const modules = import.meta.glob(`/content/**/*.{md,svx,svelte.md}`);
+	const modules = import.meta.glob(`/content/**/*.{md,svx,svelte.md}`);
+	const postPromises = Object.entries(modules).map(([path, resolver]) =>
+		resolver().then(
+			(post) =>
+				({
+					slug: slugFromPath(path).includes('index.md')
+						? slugFromPath(path).slice(0, slugFromPath(path).lastIndexOf('/'))
+						: slugFromPath(path),
+					...(post as unknown as MdsvexFile).metadata
+				}) as BlogPost
+		)
+	);
 
-  console.log(modules);
+	const posts = await Promise.all(postPromises);
 
-  const postPromises = Object.entries(modules).map(([path, resolver]) =>
-    resolver().then(
-      (post) =>
-        ({
-          slug: slugFromPath(path).includes("index.md")
-            ? slugFromPath(path).slice(0, slugFromPath(path).lastIndexOf("/"))
-            : slugFromPath(path),
-          ...(post as unknown as MdsvexFile).metadata,
-        } as BlogPost)
-    )
-  );
+	posts.sort((a, b) => (new Date(a.date) > new Date(b.date) ? -1 : 1));
 
-  const posts = await Promise.all(postPromises);
-
-  posts.sort((a, b) => (new Date(a.date) > new Date(b.date) ? -1 : 1));
-
-  return { posts };
+	return { posts };
 };
